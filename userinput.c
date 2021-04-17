fa217a174743822c40d2e244dc1e7c34c5fcd3d1
perol <khairul2564@gmail.com>
2021-04-17 15:46:07 +0800

    First Commit

---
+++ userinput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int testInteger;
+    printf("Enter your age: ");
+    scanf("%d", &testInteger);  
+    printf("Your age = %d",testInteger);
+    return 0;
+}
